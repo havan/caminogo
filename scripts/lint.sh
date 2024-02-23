0792e0af858e7d986a23d03abe0e82e180627762
Mohamed Elmoslemany <117270519+mo-c4t@users.noreply.github.com>
2024-02-23 11:13:42 +0100

    [CNR] Fix workflows, exclude from linting, update CNR to avax 1.7.0 (#303)
    * move cnr workflows back to .github/workflows and run e2e tests after cnr build
    
    * [CNR] Exclude tools/camino-network-runner from linting, update CNR to avax 1.7.0
    
    ---------
    
    Co-authored-by: evlekht <ev.lekht@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -67,14 +67,14 @@ function test_license_header {
 }
 
 function test_single_import {
-  if grep -R -zo -P --exclude-dir='tools/camino-network-runner' 'import \(\n\t".*"\n\)' .; then
+  if grep -R -zo -P --exclude-dir='camino-network-runner' 'import \(\n\t".*"\n\)' .; then
     echo ""
     return 1
   fi
 }
 
 function test_require_error_is_no_funcs_as_params {
-  if grep -R -zo -P --exclude-dir='tools/camino-network-runner' 'require.ErrorIs\(.+?\)[^\n]*\)\n' .; then
+  if grep -R -zo -P --exclude-dir='camino-network-runner' 'require.ErrorIs\(.+?\)[^\n]*\)\n' .; then
     echo ""
     return 1
   fi
@@ -82,7 +82,7 @@ function test_require_error_is_no_funcs_as_params {
 
 function test_require_equal_zero {
   # check if the first arg, other than t, is 0
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' 'require\.Equal\((t, )?(u?int\d*\(0\)|0)' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' 'require\.Equal\((t, )?(u?int\d*\(0\)|0)' .; then
     echo ""
     echo "Use require.Zero instead of require.Equal when testing for 0."
     echo ""
@@ -90,7 +90,7 @@ function test_require_equal_zero {
   fi
 
   # check if the last arg is 0
-  if grep -R -zo -P --exclude-dir='tools/camino-network-runner' 'require\.Equal\(.+?, (u?int\d*\(0\)|0)\)\n' .; then
+  if grep -R -zo -P --exclude-dir='camino-network-runner' 'require\.Equal\(.+?, (u?int\d*\(0\)|0)\)\n' .; then
     echo ""
     echo "Use require.Zero instead of require.Equal when testing for 0."
     echo ""
@@ -99,7 +99,7 @@ function test_require_equal_zero {
 }
 
 function test_require_len_zero {
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' 'require\.Len\((t, )?.+, 0(,|\))' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' 'require\.Len\((t, )?.+, 0(,|\))' .; then
     echo ""
     echo "Use require.Empty instead of require.Len when testing for 0 length."
     echo ""
@@ -117,7 +117,7 @@ function test_require_equal_len {
   # These should match:
   # - require.Equal(2, len(foo))
   # - require.Equal(t, 2, len(foo))
-  if grep -R -o -P --exclude-dir='scripts' --exclude-dir='tools/camino-network-runner' 'require\.Equal\((t, )?.*, len\([^,]*$' .; then
+  if grep -R -o -P --exclude-dir='scripts' --exclude-dir='camino-network-runner' 'require\.Equal\((t, )?.*, len\([^,]*$' .; then
     echo ""
     echo "Use require.Len instead of require.Equal when testing for length."
     echo ""
@@ -126,21 +126,21 @@ function test_require_equal_len {
 }
 
 function test_require_nil {
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' 'require\..+?!= nil' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' 'require\..+?!= nil' .; then
     echo ""
     echo "Use require.NotNil when testing for nil inequality."
     echo ""
     return 1
   fi
 
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' 'require\..+?== nil' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' 'require\..+?== nil' .; then
     echo ""
     echo "Use require.Nil when testing for nil equality."
     echo ""
     return 1
   fi
 
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' 'require\.ErrorIs.+?nil\)' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' 'require\.ErrorIs.+?nil\)' .; then
     echo ""
     echo "Use require.NoError instead of require.ErrorIs when testing for nil error."
     echo ""
@@ -149,7 +149,7 @@ function test_require_nil {
 }
 
 function test_require_no_error_inline_func {
-  if grep -R -zo -P --exclude-dir='tools/camino-network-runner' '\t+err :?= ((?!require|if).|\n)*require\.NoError\((t, )?err\)' .; then
+  if grep -R -zo -P --exclude-dir='camino-network-runner' '\t+err :?= ((?!require|if).|\n)*require\.NoError\((t, )?err\)' .; then
     echo ""
     echo "Checking that a function with a single error return doesn't error should be done in-line."
     echo ""
@@ -159,7 +159,7 @@ function test_require_no_error_inline_func {
 
 # Ref: https://go.dev/doc/effective_go#blank_implements
 function test_interface_compliance_nil {
-  if grep -R -o -P --exclude-dir='tools/camino-network-runner' '_ .+? = &.+?\{\}' .; then
+  if grep -R -o -P --exclude-dir='camino-network-runner' '_ .+? = &.+?\{\}' .; then
     echo ""
     echo "Interface compliance checks need to be of the form:"
     echo "  var _ json.Marshaler = (*RawMessage)(nil)"
