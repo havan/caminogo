438f1633502ac2bd300144490e2323c12d4b440c
evlekht <ev.lekht@gmail.com>
2024-05-16 20:23:41 +0400

    wip
    # Conflicts:
    #	vms/platformvm/camino_client.go

--- vms/platformvm/camino_client.go
+++ vms/platformvm/camino_client.go
@@ -7,6 +7,8 @@ import (
 	"context"
 
 	"github.com/ava-labs/avalanchego/api"
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/formatting"
 	"github.com/ava-labs/avalanchego/utils/rpc"
 )
 
@@ -16,6 +18,11 @@ type CaminoClient interface {
 
 	// GetMultisigAlias returns the alias definition of the given multisig address
 	GetMultisigAlias(ctx context.Context, multisigAddress string, options ...rpc.Option) (*GetMultisigAliasReply, error)
+
+	GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error)
+
+	GetRegisteredShortIDLink(ctx context.Context, addrStr ids.ShortID, options ...rpc.Option) (string, error)
+	GetLastAcceptedBlock(ctx context.Context, encoding formatting.Encoding, options ...rpc.Option) ([]byte, error)
 }
 
 func (c *client) GetConfiguration(ctx context.Context, options ...rpc.Option) (*GetConfigurationReply, error) {
@@ -30,4 +37,26 @@ func (c *client) GetMultisigAlias(ctx context.Context, multisigAddress string, o
 		Address: multisigAddress,
 	}, res, options...)
 	return res, err
+}
+
+func (c *client) GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error) {
+	res := &GetAllDepositOffersReply{}
+	err := c.requester.SendRequest(ctx, "platform.getAllDepositOffers", &getAllDepositOffersArgs, res, options...)
+	return res, err
+}
+
+func (c *client) GetRegisteredShortIDLink(ctx context.Context, addrStr ids.ShortID, options ...rpc.Option) (string, error) {
+	res := &api.JSONAddress{}
+	err := c.requester.SendRequest(ctx, "platform.getMultisigAlias", &api.JSONAddress{
+		Address: addrStr.String(),
+	}, res, options...)
+	return res.Address, err
+}
+
+func (c *client) GetLastAcceptedBlock(ctx context.Context, encoding formatting.Encoding, options ...rpc.Option) ([]byte, error) {
+	res := &api.GetBlockResponse{}
+	err := c.requester.SendRequest(ctx, "platform.getLastAcceptedBlock", &api.Encoding{
+		Encoding: encoding,
+	}, res, options...)
+	return res.Block, err
 }
