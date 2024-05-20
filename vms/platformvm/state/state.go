162791ec9254b6e2f051a55687cdc486d7801a76
evlekht <ev.lekht@gmail.com>
2024-05-20 14:52:37 +0400

    [PVM] replace nil-signers Sign with Initialize, make syncGenesis private

--- vms/platformvm/state/state.go
+++ vms/platformvm/state/state.go
@@ -1838,7 +1838,7 @@ func (s *state) init(genesisBytes []byte) error {
 		return err
 	}
 
-	if err := s.caminoState.SyncGenesis(s, genesis); err != nil {
+	if err := s.caminoState.syncGenesis(s, genesis); err != nil {
 		return err
 	}
 
