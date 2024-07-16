739da73a9908ca1c3934eed928e40e88ff7da72d
evlekht <ev.lekht@gmail.com>
2024-07-16 19:14:43 +0400

    wip

--- version/constants.go
+++ version/constants.go
@@ -84,99 +84,106 @@ var (
 
 	DefaultUpgradeTime = time.Date(2020, time.December, 5, 5, 0, 0, 0, time.UTC)
 
+	caminoEarliestTime     = time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC)
+	columbusEarliestTime   = time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC)
+	kopernikusEarliestTime = time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC)
+
+	unreachableFutureTime = time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC)
+
 	ApricotPhase1Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2021, time.March, 31, 14, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2021, time.March, 26, 14, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhase2Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2021, time.May, 10, 11, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2021, time.May, 5, 14, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhase3Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2021, time.August, 24, 14, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2021, time.August, 16, 19, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhase4Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2021, time.September, 22, 21, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2021, time.September, 16, 21, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
-	// TODO@ !!!! possibly was missing for our go-live and still missing!
-	// TODO@ might affect existing network!!!
-	// TODO@ NOPE. default 0 is totally fine.
 	ApricotPhase4MinPChainHeight = map[uint32]uint64{
 		constants.MainnetID: 793005,
 		constants.FujiID:    47437,
+
+		constants.CaminoID:     0,
+		constants.ColumbusID:   0,
+		constants.KopernikusID: 0,
 	}
 
 	ApricotPhase5Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2021, time.December, 2, 18, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2021, time.November, 24, 15, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhasePre6Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.September, 5, 1, 30, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2022, time.September, 6, 20, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	SunrisePhase0Times = map[uint32]time.Time{
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhase6Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.September, 6, 20, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2022, time.September, 6, 20, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	ApricotPhasePost6Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.September, 7, 3, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2022, time.September, 7, 6, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	BanffTimes = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.October, 18, 16, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(2022, time.October, 3, 14, 0, 0, 0, time.UTC),
 
-		constants.CaminoID:     time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
-		constants.KopernikusID: time.Date(2022, time.May, 16, 8, 0, 0, 0, time.UTC),
+		constants.CaminoID:     caminoEarliestTime,
+		constants.ColumbusID:   columbusEarliestTime,
+		constants.KopernikusID: kopernikusEarliestTime,
 	}
 
 	AthensPhaseTimes = map[uint32]time.Time{
@@ -208,9 +215,9 @@ var (
 		constants.MainnetID: time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
 		constants.FujiID:    time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
 
-		constants.KopernikusID: time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
-		constants.ColumbusID:   time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
-		constants.CaminoID:     time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
+		constants.KopernikusID: unreachableFutureTime,
+		constants.ColumbusID:   unreachableFutureTime,
+		constants.CaminoID:     unreachableFutureTime,
 	}
 )
 
