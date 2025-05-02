dc19755a06d89fd6238bba37a752740c68bd176b
evlekht <ev.lekht@gmail.com>
2025-05-02 17:47:10 +0400

    [PVM] update client methods (#416)

--- vms/platformvm/camino_client.go
+++ vms/platformvm/camino_client.go
@@ -6,8 +6,10 @@ package platformvm
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ava-labs/avalanchego/api"
+	"github.com/ava-labs/avalanchego/codec"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/formatting"
 	"github.com/ava-labs/avalanchego/utils/formatting/address"
@@ -15,9 +17,15 @@ import (
 	"github.com/ava-labs/avalanchego/utils/rpc"
 	as "github.com/ava-labs/avalanchego/vms/platformvm/addrstate"
 	platformapi "github.com/ava-labs/avalanchego/vms/platformvm/api"
+	"github.com/ava-labs/avalanchego/vms/platformvm/deposit"
 	"github.com/ava-labs/avalanchego/vms/platformvm/state"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 )
+
+type DepositWithAvailableReward struct {
+	Deposit         deposit.Deposit
+	AvailableReward uint64
+}
 
 type CaminoClient interface {
 	// GetConfiguration returns genesis information of the primary network
@@ -26,13 +34,15 @@ type CaminoClient interface {
 	// GetMultisigAlias returns the alias definition of the given multisig address
 	GetMultisigAlias(ctx context.Context, multisigAddress string, options ...rpc.Option) (*GetMultisigAliasReply, error)
 
-	GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error)
+	GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) ([]*deposit.Offer, error)
 
-	GetRegisteredShortIDLink(ctx context.Context, addrStr ids.ShortID, options ...rpc.Option) (string, error)
+	GetRegisteredShortIDLink(ctx context.Context, addrOrNodeID string, options ...rpc.Option) (ids.ShortID, error)
 	GetLastAcceptedBlock(ctx context.Context, encoding formatting.Encoding, options ...rpc.Option) (any, error)
 	GetBlockAtHeight(ctx context.Context, height uint32, encoding formatting.Encoding, options ...rpc.Option) (any, error)
 	GetClaimables(ctx context.Context, owners []*secp256k1fx.OutputOwners, options ...rpc.Option) ([]*state.Claimable, error)
 	GetAddressStates(ctx context.Context, addr ids.ShortID, options ...rpc.Option) (as.AddressState, error)
+
+	GetDeposits(ctx context.Context, depositTxIDs []ids.ID, options ...rpc.Option) ([]DepositWithAvailableReward, time.Time, error)
 }
 
 func (c *client) GetConfiguration(ctx context.Context, options ...rpc.Option) (*GetConfigurationReply, error) {
@@ -49,18 +59,74 @@ func (c *client) GetMultisigAlias(ctx context.Context, multisigAddress string, o
 	return res, err
 }
 
-func (c *client) GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error) {
+func (c *client) GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) ([]*deposit.Offer, error) {
 	res := &GetAllDepositOffersReply{}
 	err := c.requester.SendRequest(ctx, "platform.getAllDepositOffers", &getAllDepositOffersArgs, res, options...)
-	return res, err
+	if err != nil {
+		return nil, err
+	}
+
+	offers := make([]*deposit.Offer, len(res.DepositOffers))
+	for i, apiOffer := range res.DepositOffers {
+		offers[i] = offerFromAPI(apiOffer)
+	}
+
+	return offers, nil
 }
 
-func (c *client) GetRegisteredShortIDLink(ctx context.Context, addr ids.ShortID, options ...rpc.Option) (string, error) {
+func (c *client) GetRegisteredShortIDLink(ctx context.Context, addrOrNodeID string, options ...rpc.Option) (ids.ShortID, error) {
 	res := &api.JSONAddress{}
 	err := c.requester.SendRequest(ctx, "platform.getRegisteredShortIDLink", &api.JSONAddress{
-		Address: addr.String(),
+		Address: addrOrNodeID,
 	}, res, options...)
-	return res.Address, err
+	if err != nil {
+		return ids.ShortEmpty, err
+	}
+	return ids.ShortFromString(res.Address)
+}
+
+func (c *client) GetDeposits(ctx context.Context, depositTxIDs []ids.ID, options ...rpc.Option) ([]DepositWithAvailableReward, time.Time, error) {
+	res := &GetDepositsReply{}
+	err := c.requester.SendRequest(ctx, "platform.getDeposits", &GetDepositsArgs{
+		DepositTxIDs: depositTxIDs,
+	}, res, options...)
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+
+	if len(res.Deposits) != len(res.AvailableRewards) {
+		return nil, time.Time{}, fmt.Errorf("len(deposits) != len(availableRewards): %d != %d", len(res.Deposits), len(res.AvailableRewards))
+	}
+
+	deposits := make([]DepositWithAvailableReward, len(res.Deposits))
+	for i, apiDeposit := range res.Deposits {
+		deposit := &deposit.Deposit{
+			DepositOfferID:      apiDeposit.DepositOfferID,
+			UnlockedAmount:      uint64(apiDeposit.UnlockedAmount),
+			ClaimedRewardAmount: uint64(apiDeposit.ClaimedRewardAmount),
+			Start:               uint64(apiDeposit.Start),
+			Duration:            apiDeposit.Duration,
+			Amount:              uint64(apiDeposit.Amount),
+		}
+		rewardOwner := &secp256k1fx.OutputOwners{
+			Threshold: uint32(apiDeposit.RewardOwner.Threshold),
+			Addrs:     make([]ids.ShortID, len(apiDeposit.RewardOwner.Addresses)),
+		}
+		for i := range apiDeposit.RewardOwner.Addresses {
+			addr, err := address.ParseToID(apiDeposit.RewardOwner.Addresses[i])
+			if err != nil {
+				return nil, time.Time{}, err
+			}
+			rewardOwner.Addrs[i] = addr
+		}
+		deposit.RewardOwner = rewardOwner
+		deposits[i] = DepositWithAvailableReward{
+			Deposit:         *deposit,
+			AvailableReward: uint64(res.AvailableRewards[i]),
+		}
+	}
+
+	return deposits, time.Unix(int64(res.Timestamp), 0), nil
 }
 
 func (c *client) GetLastAcceptedBlock(ctx context.Context, encoding formatting.Encoding, options ...rpc.Option) (any, error) {
@@ -160,6 +226,28 @@ func secpOwnerFromAPI(apiOwner *platformapi.Owner) (*secp256k1fx.OutputOwners, e
 		return secpOwner, nil
 	}
 	return nil, nil
+}
+
+func offerFromAPI(apiOffer *APIDepositOffer) *deposit.Offer {
+	return &deposit.Offer{
+		UpgradeVersionID:        codec.UpgradeVersionID(apiOffer.UpgradeVersion),
+		ID:                      apiOffer.ID,
+		InterestRateNominator:   uint64(apiOffer.InterestRateNominator),
+		Start:                   uint64(apiOffer.Start),
+		End:                     uint64(apiOffer.End),
+		MinAmount:               uint64(apiOffer.MinAmount),
+		TotalMaxAmount:          uint64(apiOffer.TotalMaxAmount),
+		DepositedAmount:         uint64(apiOffer.DepositedAmount),
+		MinDuration:             apiOffer.MinDuration,
+		MaxDuration:             apiOffer.MaxDuration,
+		UnlockPeriodDuration:    apiOffer.UnlockPeriodDuration,
+		NoRewardsPeriodDuration: apiOffer.NoRewardsPeriodDuration,
+		Memo:                    apiOffer.Memo,
+		Flags:                   deposit.OfferFlag(apiOffer.Flags),
+		TotalMaxRewardAmount:    uint64(apiOffer.TotalMaxRewardAmount),
+		RewardedAmount:          uint64(apiOffer.RewardedAmount),
+		OwnerAddress:            apiOffer.OwnerAddress,
+	}
 }
 
 func parseAddr(addrStr string) (ids.ShortID, error) {
